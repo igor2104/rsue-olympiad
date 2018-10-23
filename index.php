200b8ee797a3b791467fcd4e2377577af393d4db
Игорь Ермолов <ermolov962104@gmail.com>
2018-10-23 17:28:22 +0300

    init project

---
+++ index.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Document</title>
+
+	<link rel="stylesheet" type="text/css" href="lib/bootstrap-4/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/fonts.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/media.css">
+</head>
+<body>
+
+	<header class="header">
+		<div class="container">
+			<div class="row nav">
+				<div class="col-12 col-lg-4 text-center mb-3 mb-lg-0">
+					<div class="row">
+						<div class="col-4"><img src="img/logo-1.png" alt=""></div>
+						<div class="col-4"><img src="img/logo-2.png" alt=""></div>
+						<div class="col-4"><img src="img/logo-3.png" alt=""></div>
+					</div>
+				</div>
+				<div class="col-12 offset-lg-1 col-lg-4 address text-center text-lg-left mb-3 mb-lg-0">
+					г. Ростов-на-Дону,<br>
+					ул. Большая Садовая, д. 69
+				</div>
+				<div class="col-12 col-lg-3 text-center text-lg-right">
+					<div><a href="tel:(863) 240-29-86" class="phone">(863) 240-29-86</a></div>
+					<div><a href="mailto:rsueuef509@yandex.ru" class="mail">rsueuef509@yandex.ru</a></div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12">
+					<div class="img"></div>
+					<div class="row">
+						<div class="col-12 col-lg-8 col-xl-7 text-center text-lg-left substrate">
+							<h1>Ежегодная Всероссийская онлайн-Олимпиада по бухгалтерскому учету</h1>
+							<a href="" class="btn-reg">Принять участие</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12 term text-center text-lg-left">
+					Срок регистрации: 14 ноября 2018 г.
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12 info text-center text-lg-left">
+					Участие в Олимпиаде осуществляется на безвозмездной основе.
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<main class="content-index">
+		<div class="bg-gray wrapper">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<div class="info-block-head">
+							Результаты ежегодной Всероссийской онлайн-Олимпиады будут опубликованы в журнале «Учет и статистика», включенном в перечень научных изданий рецензируемых ВАК
+						</div>
+					</div>
+				</div>
+
+				<div class="row info-block d-flex">
+					<div class="col-md-6 pr-md-0 d-flex">
+						<div class="item-1">
+							ФГБОУ ВО «РГЭУ (РИНХ)» проводит I Всероссийскую онлайн Олимпиаду по бухгалтерскому учету среди студентов вузов, обучающихся по направлению:
+						</div>
+
+					</div>
+					<div class="col-md-6 pl-md-0 d-flex">
+						<div class="item-2">
+							<h4><span class="number">38.03.01</span> «Экономика»</h4>
+							<p>по профилям в области бухгалтерского учета, анализа и аудита (уровень обучения бакалавриат). </p>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-12 text-center text-md-left">
+						<h2 class="subheading">
+							Олимпиада включает два этапа, проводимые в заочной форме с применением дистанционных образовательных технологий:
+						</h2>
+					</div>
+				</div>
+
+				<div class="row block-text text-justify text-md-left">
+					<div class="col-md-6 mb-4 mb-md-0">
+						<span class="bold">Первый этап</span> представляет собой отборочный тур и предполагает выполнение индивидуальных тестовых заданий. Все участники первого этапа Олимпиады получают Сертификат установленного образца.
+
+					</div>
+					<div class="col-md-6">
+						<p>
+							<span class="bold">Второй этап</span> к участию допускаются участники Первого этапа Олимпиады, набравшие наибольшее количество баллов. Второй этап предполагает выполнение индивидуальных тестовых заданий повышенного уровня сложности и решение кейс-задания.
+						</p>
+						<div class="row">
+							<div class="col-sm-6">
+								Победители Второго этапа Олимпиады получают
+								Дипломы Победителей
+								(1-е, 2-е и 3-е место).
+							</div>
+							<div class="col-sm-6">
+								Участники, набравшие наибольшее количество баллов после победителей, получают Дипломы Лауреатов.
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-12">
+						<hr class="hr">
+					</div>
+				</div>
+				<div class="row block-text text-justify text-md-left">
+					<div class="col-12">
+						По итогам проведения Олимпиады по бухгалтерскому учету научным руководителям студентов, получивших Дипломы Победителей и Дипломы Лауреатов, выдаются Грамоты за руководство студентами, показавшими высокие результаты по итогам I Всероссийской онлайн Олимпиады по бухгалтерскому учету под руководством научно-педагогических работников.
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="wrapper-2">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<h2 class="heading text-center">Оргкомитет Олимпиады</h2>
+					</div>
+				</div>
+
+				<div class="row text-center block-comitet">
+					<div class="col-sm-6 col-md-4 item">
+						<img src="img/albekov.png" alt="">
+						<h6>А.У. Альбеков</h6>
+						<p>
+							Председатель:
+							И.о. ректора РГЭУ (РИНХ), д.э.н., профессор,
+							заслуженный деятель науки РФ,
+							член Совета при Президенте РФ
+							по межнациональным отношениям
+						</p>
+					</div>
+					<div class="col-sm-6 col-md-4 item">
+						<img src="img/labincev.png" alt="">
+						<h6>Н.Т. Лабынцев</h6>
+						<p>
+							Зам. председателя Оргкомитета:
+							зав. кафедрой
+							бухгалтерского учета
+							РГЭУ (РИНХ)
+							д.э.н., профессор
+						</p>
+					</div>
+					<div class="col-sm-6 col-md-4 item">
+						<img src="img/makarenko.png" alt="">
+						<h6>Е.Н. Макаренко</h6>
+						<p>
+							Члены Оргкомитета:
+							Первый проректор - проректор
+							по учебной работе РГЭУ (РИНХ),
+							д.э.н., профессор
+						</p>
+					</div>
+					<div class="col-sm-6 col-md-4 item">
+						<img src="img/kislaya.png" alt="">
+						<h6>И.А. Кислая</h6>
+						<p>
+							И.о. декана учетно-экономического
+							факультета РГЭУ (РИНХ), к.э.н., доцент
+						</p>
+					</div>
+					<div class="col-sm-6 col-md-4 item">
+						<img src="img/omelchinko.png" alt="">
+						<h6>И.А. Омельченко</h6>
+						<p>
+							к.э.н., доцент кафедры бухгалтерского учета
+						</p>
+					</div>
+					<div class="col-sm-6 col-md-4 item">
+						<img src="img/andreeva.png" alt="">
+						<h6>Н.А. Андреева</h6>
+						<p>
+							к.э.н., доцент кафедры бухгалтерского учета
+						</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-12">
+						<h2 class="heading text-center">Аттестационная комиссия Олимпиады</h2>
+					</div>
+				</div>
+				<div class="block-commission">
+					<div class="row margin-bottom">
+						<div class="col-12">
+							<h4 class="subheading text-center">Председатель</h2>
+						</div>
+						<div class="offset-1 col-sm-3">
+							<strong>Н.Т. Лабынцев</strong>
+						</div>
+						<div class="offset-1 col-sm-6">
+							<p>зав. кафедрой бухгалтерского учета РГЭУ (РИНХ) д.э.н., профессор</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-12">
+							<h4 class="subheading text-center">Члены Комиссии</h2>
+						</div>
+						<div class="offset-1 col-sm-3">
+							<strong>И.А. Кислая</strong>
+						</div>
+						<div class="offset-1 col-sm-6">
+							<p>И.о. декана учетно-экономического факультета РГЭУ (РИНХ), к.э.н., доцент</p>
+						</div>
+						<div class="offset-1 col-10">
+							<hr class="hr">
+						</div>
+						<div class="offset-1 col-sm-3">
+							<strong>Н.Н. Хахонова</strong>
+						</div>
+						<div class="offset-1 col-sm-6">
+							<p>д.э.н., профессор кафедры бухгалтерского учета</p>
+						</div>
+						<div class="offset-1 col-10">
+							<hr class="hr">
+						</div>
+						<div class="offset-1 col-sm-3">
+							<strong>Ю.Г. Чернышева</strong>
+						</div>
+						<div class="offset-1 col-sm-6">
+							<p>д.э.н., профессор кафедры анализа хозяйственной деятельности и прогнозирования</p>
+						</div>
+						<div class="offset-1 col-10">
+							<hr class="hr">
+						</div>
+						<div class="offset-1 col-sm-3">
+							<strong>И.А. Омельченко</strong>
+						</div>
+						<div class="offset-1 col-sm-6">
+							<p>к.э.н., доцент кафедры бухгалтерского учета</p>
+						</div>
+						<div class="offset-1 col-10">
+							<hr class="hr">
+						</div>
+						<div class="offset-1 col-sm-3">
+							<strong>Н.А. Андреева</strong>
+						</div>
+						<div class="offset-1 col-sm-6">
+							<p>к.э.н., доцент кафедры бухгалтерского учета</p>
+						</div>
+					</div>
+				</div>
+				<div class="row d-flex align-items-end">
+					<div class="col-12">
+						<h2 class="heading text-center">Контактная информация</h2>
+					</div>
+					<div class="col-md-4 text-center text-md-left address mb-2 mb-md-0">
+						г. Ростов-на-Дону,<br>
+						ул. Большая Садовая, д. 69
+					</div>
+					<div class="col-md-4 text-center mb-2 mb-md-0">
+						<a href="mailto:rsueuef509@yandex.ru" class="mail">rsueuef509@yandex.ru</a>
+					</div>
+					<div class="col-md-4 text-center text-md-right phone">
+						<a href="tel:(863) 240-29-86" class="phone">(863) 240-29-86</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+
+	<footer class="footer w-100">
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10838.428029157232!2d39.71517559915521!3d47.2242713831553!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40e3b9a8f82b0a17%3A0xc33cdad55cafa7b2!2z0KDQvtGB0YLQvtCy0YHQutC40Lkg0LPQvtGB0YPQtNCw0YDRgdGC0LLQtdC90L3Ri9C5INGN0LrQvtC90L7QvNC40YfQtdGB0LrQuNC5INGD0L3QuNCy0LXRgNGB0LjRgtC10YIgKNCg0JjQndClKQ!5e0!3m2!1sru!2sru!4v1521798653072" width="100%" height="672" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+	</footer>
+</body>
+</html>
